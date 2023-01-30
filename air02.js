4ee9d7c09d2da9047f80375f706721af575f1d11
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2023-01-30 10:58:07 +0100

    Ajout de air02.js

---
+++ air02.js
@@ -0,0 +1,68 @@
+///////////////// Concat ////////////////
+
+
+// Functions:
+
+function CreateArray(arrayInput){
+    if(NoError(arrayInput)){
+        let newArray = [];
+
+        for(i = 2; i < arrayInput.length; i++){
+            newArray.push(arrayInput[i]);
+        }
+    
+        return newArray;
+    }else{
+        return "Error!";
+    }
+}
+
+
+function Concat(array, separator){
+    if(array == "Error!"){
+        return array;
+    }else{
+        let string = "";
+    
+        for(i = 0; i < array.length -1; i++){
+            string += array[i] + separator;
+        }
+
+        return string;
+    }
+}
+
+
+function ShowString(string){
+    console.log(string);
+}
+
+
+// Error:
+
+function NoError(arrayInput){
+    if(arrayInput.length >= 4){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+
+// Parsing:
+
+const pathArray = process.argv;
+let stringArray = CreateArray(pathArray);
+let separatorInput = stringArray[stringArray.length -1];
+
+
+// Resolve:
+
+let newString = Concat(stringArray, separatorInput);
+
+
+// Display:
+
+ShowString(newString);
