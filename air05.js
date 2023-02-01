c1afbb975d68732d3517535804a3da72b321cbb3
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2023-02-01 11:15:47 +0100

    Modif: Ajout de fonctions à air05.js

--- air05.js
+++ air05.js
@@ -5,25 +5,36 @@
 
 function NewArray(inputPath){
     let tempArray = [];
-    for(i = 2; i < inputPath.length; i++){
+    for(i = 2; i < inputPath.length -1; i++){
         tempArray.push(inputPath[i]);
     }
     return tempArray;
 }
 
 
-function AddOrSub(nbrList, operator, operatorNumber){
+function OperationResults(resultArray){
+    let tempResult = "";
+
+    for(i = 0; i < resultArray.length; i++){
+        tempResult += resultArray[i] + " ";
+    }
+
+    return tempResult;
+}
+
+
+function AddOrSub(nbrList, operatorSign, operatorNumber){
     let result = [];
 
     // Addition
-    if(operator == "+"){
+    if(operatorSign == "+"){
         for(i = 0; i < nbrList.length; i++){
             result.push(Addition(nbrList[i], operatorNumber));
         }
         return result;
-        
+
     // Subtraction    
-    }else if(operator == "-"){
+    }else if(operatorSign == "-"){
         for(i = 0; i < nbrList.length; i++){
             result.push(Subtraction(nbrList[i], operatorNumber));
         }
@@ -34,9 +45,29 @@ function AddOrSub(nbrList, operator, operatorNumber){
 
 
 function Addition(nbr1, nbr2){
-    return nbr1 + nbr2;
+    return parseInt(nbr1) + parseInt(nbr2);
 }
 
 function Subtraction(nbr1, nbr2){
-    return nbr1 - nbr2;
+    return parseInt(nbr1) - parseInt(nbr2);
 }
+
+
+// Error:
+
+
+// Parsing:
+
+const path = process.argv;
+let inputNumbers = NewArray(path);
+let opeSign = path[path.length-1].charAt(0);
+let opeNbr = path[path.length-1].substring(1);
+
+
+// Resolve:
+
+let results = OperationResults(AddOrSub(inputNumbers, opeSign, opeNbr));
+
+// Display:
+
+console.log(results);
