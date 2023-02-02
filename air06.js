c3f238531c2ef144714f51c0421373f2c28d9100
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2023-02-02 23:02:27 +0100

    Ajout de air06.js

---
+++ air06.js
@@ -0,0 +1,73 @@
+//////////////// Contôle de pass sanitaire ///////////////
+
+
+// Functions:
+
+function NewArray(inputPath){
+    let tempArray = [];
+    for(i = 2; i < inputPath.length -1; i++){
+        tempArray.push(inputPath[i]);
+    }
+    return tempArray;
+}
+
+
+function IsStringInArray(inputArray, string){
+    
+    if(IsEnoughArg(inputArray)){
+        let newStringArray = [];
+
+        for(i = 0; i < inputArray.length; i++){
+            let item = inputArray[i];
+
+            // Check if there is correspondence:
+            if(item.indexOf(string) != -1){
+                newStringArray.push(item);
+            }
+        }
+        return newStringArray;
+
+    }else{
+        return "Error!"
+    }
+}
+
+
+function ArrayToString(outputArray){
+    if(outputArray == "Error!"){
+        return outputArray;
+    }else{
+        let tempString = "";
+    
+        for(i = 0; i < outputArray.length; i++){
+            tempString += outputArray[i] + " "; 
+        }
+        return tempString;
+    }
+}
+
+// Errors:
+
+function IsEnoughArg(inputArray){
+    if(inputArray.length >= 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+// Parsing:
+
+const path = process.argv;
+let inputArray = NewArray(path);
+let refString = path[path.length-1];
+
+// Resolve:
+
+let newArray = IsStringInArray(inputArray, refString);
+
+// Display:
+
+console.log(ArrayToString(newArray));
