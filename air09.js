c5940f2c7ced251428532d198a532c411f16e899
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2023-02-09 16:32:52 +0100

    Ajout de air09.js

---
+++ air09.js
@@ -0,0 +1,71 @@
+/////////////// Rotation vers la gauche /////////////
+
+
+// Functions: 
+
+function CreateArray(inputPath){
+    if(IsEnoughArg(inputPath)){
+        let tempArray = [];
+
+        for(i = 2; i < inputPath.length; i++){
+            tempArray.push(inputPath[i]);
+        }
+        return tempArray;
+    }else{
+        return false;
+    }
+}
+
+function LeftRotation(array){
+    if(array){
+        let tempArray = array;
+        let firstItem = tempArray.shift();
+
+        tempArray.push(firstItem);
+
+        return tempArray;
+    }else{
+        return "Error!";
+    }
+}
+
+function ArrayToString(outputArray){
+    if(outputArray == "Error!"){
+        return outputArray;
+    }else{
+        let newList = "";
+
+        for(i = 0; i < outputArray.length; i++){
+            if(i < outputArray.length -1){
+                newList += `${outputArray[i]}, `;
+            }else if(i == outputArray.length -1){
+                newList += `${outputArray[i]}`;
+            }
+             
+        }
+        return newList;
+    }
+}
+// Errors:
+
+function IsEnoughArg(inputPath){
+    if(inputPath.length >= 4){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// Parsing:
+
+const path = process.argv;
+let myArray = CreateArray(path);
+
+
+// Resolve:
+
+myNewArray = LeftRotation(myArray);
+
+// Display:
+
+console.log(ArrayToString(myNewArray));
